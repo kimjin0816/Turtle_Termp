756966a02abe8c56dc1e6b9d37da92195c8109d6
kimjin0816 <qorwh0816@naver.com>
2024-01-08 11:23:57 +0900

    To-do list commit

--- backend/clothesTable_model.py
+++ backend/clothesTable_model.py
@@ -4,38 +4,39 @@ class clothesTable_model:
     def __init__(self):
         self.conn = None
         self.cur = None
+        self.first_letter = None
 #-----------------------------------------------------------------------------------------------------------------------------------------------
-#region Methhod
+#region DB connection/close/write methods
     # DB connection
     def connectDb(self):
         self.conn = psycopg2.connect(host='localhost', database='Clothes', user='postgres', password='password', port='5432')
         self.cur = self.conn.cursor()
-        
+    # DB close
     def closeDb(self):
         self.cur.close()
         self.conn.close()
-        
+    # DB write   
     def writeDb(self, cmd): 
         sCmd = str(cmd) 
         self.cur.execute(sCmd)
         self.conn.commit()
 #endregion
 #-----------------------------------------------------------------------------------------------------------------------------------------------
-    # DB table creation
+    # DB table settings
     def setTable(self):
         self.connectDb()
-        # clothe_type = '''
-        #     CREATE TYPE clothe_type AS ENUM (
-        #         'top',
-        #         'bottom',
-        #         'outer',
-        #         'dress',
-        #         'shoes',
-        #         'bag',
-        #         'accessory',
-        #         'etc'
-        #     );
-        #         '''
+        clothes_type = '''
+            CREATE TYPE clothe_type AS ENUM (
+                'top',
+                'bottom',
+                'outer',
+                'dress',
+                'shoes',
+                'bag',
+                'accessory',
+                'etc'
+            );
+                '''
         clothes_setting = '''
             CREATE TABLE IF NOT EXISTS clothing_type (
                 type_id SERIAL PRIMARY KEY,
@@ -46,15 +47,14 @@ class clothesTable_model:
             VALUES ('long'), ('short')
             ON CONFLICT (type_name) DO NOTHING;
 
-            CREATE SEQUENCE neat_sequence START 1;
-            CREATE SEQUENCE hood_sequence START 1;
-            CREATE SEQUENCE shirt_sequence START 1;
-            CREATE SEQUENCE mantoman_sequence START 1;
+            CREATE SEQUENCE IF NOT EXISTS neat_sequence START 1;
+            CREATE SEQUENCE IF NOT EXISTS hood_sequence START 1;
+            CREATE SEQUENCE IF NOT EXISTS shirt_sequence START 1;
+            CREATE SEQUENCE IF NOT EXISTS mantoman_sequence START 1;
             '''
-          
         high_table = '''
             CREATE TABLE IF NOT EXISTS neat (
-                n_code VARCHAR(10) PRIMARY KEY,
+                n_code CHAR(50) PRIMARY KEY,
                 n_userId CHAR(20) NOT NULL,
                 n_type_id INT NOT NULL,
                 n_brand VARCHAR(20) NOT NULL,
@@ -63,7 +63,7 @@ class clothesTable_model:
                 n_url VARCHAR(300),
                 FOREIGN KEY (n_type_id) REFERENCES clothing_type (type_id));
             CREATE TABLE IF NOT EXISTS  mantoman (
-                m_code VARCHAR(10) PRIMARY KEY,
+                m_code CHAR(50) PRIMARY KEY,
                 m_userId CHAR(20) NOT NULL,
                 m_type_id INT NOT NULL,
                 m_brand VARCHAR(20) NOT NULL,
@@ -72,7 +72,7 @@ class clothesTable_model:
                 m_url VARCHAR(300),
                 FOREIGN KEY (m_type_id) REFERENCES clothing_type (type_id));
             CREATE TABLE IF NOT EXISTS  shirt (
-                s_code VARCHAR(10) PRIMARY KEY,
+                s_code CHAR(50) PRIMARY KEY,
                 s_userId CHAR(20) NOT NULL,
                 s_type_id INT NOT NULL,
                 s_brand VARCHAR(20) NOT NULL,
@@ -81,7 +81,7 @@ class clothesTable_model:
                 s_url VARCHAR(300),
                 FOREIGN KEY (s_type_id) REFERENCES clothing_type (type_id));
             CREATE TABLE IF NOT EXISTS  hood (
-                h_code VARCHAR(10) PRIMARY KEY,
+                h_code CHAR(50) PRIMARY KEY,
                 h_userId CHAR(20) NOT NULL,
                 h_type_id INT NOT NULL,
                 h_brand VARCHAR(20) NOT NULL,
@@ -90,62 +90,6 @@ class clothesTable_model:
                 h_url VARCHAR(300),
                 FOREIGN KEY (h_type_id) REFERENCES clothing_type (type_id));
             '''
-        
         self.writeDb(clothes_setting)
         self.writeDb(high_table)
-        self.closeDb()
-#-----------------------------------------------------------------------------------------------------------------------------------------------
-    # DB table/data deletion
-    def deleteData(self, clothes_type):
-        self.connectDb()
-        self.writeDb('DELETE FROM {clothes_type};')
-        self.closeDb()
-
-    def truncateData(self, clothes_type):
-        self.connectDb()
-        self.writeDb('TRUNCATE TABLE {clothes_type};')
-        self.closeDb()
-
-    def dropTable(self, clothes_type):
-        self.connectDb()
-        self.writeDb('DROP TABLE IF EXISTS {clothes_type};')
-        self.closeDb()
-
-    def deleteSequence(self, clothes_type):
-        self.connectDb()
-        self.writeDb('DROP SEQUENCE IF EXISTS neat_sequence;')
-        self.writeDb('DROP SEQUENCE IF EXISTS hood_sequence;')
-        self.writeDb('DROP SEQUENCE IF EXISTS shirt_sequence;')
-        self.writeDb('DROP SEQUENCE IF EXISTS mantoman_sequence;')
-        self.closeDb()
-# -----------------------------------------------------------------------------------------------------------------------------------------------
-    # DB data insertion
-    def insertData(self, clothes_type, userId, type_id, brand, color, logo, url):
-        self.connectDb()
-        self.writeDb(f"INSERT INTO {clothes_type} (n_code, n_userId, n_type_id, n_brand, n_color, n_logo, n_url) 
-                     VALUES ('n' || nextval('neat_n_code_seq'), '{userId}', '{type_id}', '{brand}', '{color}', '{logo}', '{url}');")
-        self.closeDb()
-# -----------------------------------------------------------------------------------------------------------------------------------------------
-    # DB data update 
-    # research
-    def updateData(self, clothes_type, data):
-        self.connectDb()
-        self.writeDb(f"UPDATE {clothes_type} SET n_type_id='{data[1]}'n_brand='{data[2]}', n_color='{data[3]}', n_logo='{data[4]}', n_url='{data[5]}' 
-                     WHERE n_code='{data[0]}';")
-        self.closeDb()
-#-----------------------------------------------------------------------------------------------------------------------------------------------
-    # DB one data selection
-    def oneSelectClothes(self, clothes_type, code):
-        self.connectDb()
-        self.writeDb(f"SELECT * FROM {clothes_type} WHERE n_code='{code}';")
-        result = self.cur.fetchall()
-        print(result)
-        self.closeDb()
-
-    # DB longClothes selection
-    def clothesType(self, clothes_type, type_id):
-        self.connectDb()
-        self.writeDb(f"SELECT * FROM {clothes_type} WHERE type_id= {type_id};")
-        result = self.cur.fetchall()
-        print(result)
         self.closeDb()
