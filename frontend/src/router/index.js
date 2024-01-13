c097be51e09e8cf60bf19135784f162913679cc4
kimjin0816 <qorwh0816@naver.com>
2024-01-13 12:40:31 +0900

    main에서 다시받기

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,63 +1,60 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import AboutView from '../views/AboutView.vue';
-import CodyView from '../views/CodyView.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import CodyView from "../views/CodyView.vue";
 
-import SignUp from '../views/SignUp.vue';
-import AboutView from '../views/AboutView.vue'; // AboutView 추가
-import FindCre from '@/views/FindCre.vue';
-import EditinfView from '@/views/EditinfView.vue';
-import DeleteView from '@/views/DeleteView.vue';
-
+import SignUp from "../views/SignUp.vue";
+import AboutView from "../views/AboutView.vue"; // AboutView 추가
+import FindCre from "@/views/FindCre.vue";
+import EditinfView from "@/views/EditinfView.vue";
+import DeleteView from "@/views/DeleteView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
   },
   {
-    path: '/signup',
-    name: 'signup',
+    path: "/signup",
+    name: "signup",
     component: SignUp,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     component: AboutView,
   },
   {
-    path: '/cody',
-    name: 'cody',
+    path: "/cody",
+    name: "cody",
     component: CodyView,
   },
   {
     component: AboutView, // AboutView 추가
   },
   {
-    path: '/findCredentials',
-    name: 'FindCredentials',
+    path: "/findCredentials",
+    name: "FindCredentials",
     component: FindCre,
   },
   {
-    path: '/updateProfile',
-    name: 'updateProfile',
+    path: "/updateProfile",
+    name: "updateProfile",
     component: EditinfView,
   },
   {
-    path: '/deleteProfile',
-    name: 'deleteProfile',
+    path: "/deleteProfile",
+    name: "deleteProfile",
     component: DeleteView,
   },
-  
-
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
