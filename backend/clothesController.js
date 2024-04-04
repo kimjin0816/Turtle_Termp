8743404567ee48e740e2e98d7619adb2e5c7f524
kimjin0816 <qorwh0816@naver.com>
2024-04-04 10:34:46 +0900

    clothesController.js 수정

--- backend/clothesController.js
+++ backend/clothesController.js
@@ -1,50 +1,50 @@
 const { queryClothes } = require("./PostgreDB");
 
-// entire clothing data results
-exports.export_keyword = (req, res) => {
-  const clothes_type = req.query;
-  queryClothes("SELECT * FROM $1", [clothes_type], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    res.status(200).json(results.rows);
-    // res.status(200).send(results.rows);
-  });
-};
-
-// specific clothing data results
-exports.findOne = (req, res) => {
-  const _type = req.query.clothes_type;
-  const _code = req.query.clothes_code;
-  queryMembers(
-    "SELECT * FROM $1 WHERE id = $2",
-    [_type, _code],
-    (error, results) => {
+const clothesController = {
+  // entire clothing data results
+  export_keyword(req, res) {
+    const clothes_type = req.query;
+    queryClothes("SELECT * FROM $1", [clothes_type], (error, results) => {
       if (error) {
         throw error;
       }
       res.status(200).json(results.rows);
       // res.status(200).send(results.rows);
-    }
-  );
-};
-
-// user register clothes data results
-exports.findIdClothes = (req, res) => {
-  const mem_id = req.query.clothes.mem_id;
-  const clothes_type = req.query.clothes.clothes_type;
-  queryMembers(
-    "SELECT ($1_shape, $1_classification, $1_color) FROM $2 WHERE t_code LIKE '$3_%';",
-    [clothes_type[0], clothes_type, mem_id],
-    (error, results) => {
-      if (error) {
-        throw error;
+    });
+  },
+  // specific clothing data results
+  findOne(req, res) {
+    const _type = req.query.clothes_type;
+    const _code = req.query.clothes_code;
+    queryClothes(
+      "SELECT * FROM $1 WHERE id = $2",
+      [_type, _code],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).json(results.rows);
+        // res.status(200).send(results.rows);
       }
-      res.status(200).json(results.rows);
-    }
-  );
-};
+    );
+  },
+  // user register clothes data results
 
-exports.findfeatureCode = (req, res) => {};
+  findIdClothes(req, res) {
+    const mem_id = req.query.clothes.mem_id;
+    const clothes_type = req.query.clothes.clothes_type;
+    queryClothes(
+      "SELECT ($1_shape, $1_classification, $1_color) FROM $2 WHERE t_code LIKE '$3_%';",
+      [clothes_type[0], clothes_type, mem_id],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).json(results.rows);
+      }
+    );
+  },
+  findfeatureCode(req, res) {},
+};
 
 module.exports = clothesController;
