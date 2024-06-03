8cd79010915e397f8afb21bdc99a3e448c2b8c65
NaHoSeok <1960013@365.mokwon.ac.kr>
2024-06-03 17:49:46 +0900

    ..

--- backend_5000/detection_and_color_extraction.py
+++ backend_5000/detection_and_color_extraction.py
@@ -7,24 +7,24 @@ def detect_objects_and_extract_colors(image_path, weights_path, detect_script_pa
     # YOLOv5 객체 감지 수행
     detect_command = [
         'python', str(detect_script_path), '--source', str(image_path), '--weights', str(weights_path),
-        '--img', '750', '--conf', '0.1', '--save-txt', '--save-crop'
+        '--img', '750', '--conf', '0.2', '--save-txt', '--save-crop'
     ]
     subprocess.run(detect_command, check=True)
     
     # 객체 감지 결과 디렉토리 찾기
-    detect_dir = sorted(Path(glob.glob('C:\\Users\\user\\Desktop\\Turtle_Termp\\yolov5\\runs\\detect\\*')), key=os.path.getmtime, reverse=True)[0]
+    detect_dir = sorted(Path('C:/Users/user/Desktop/Turtle_Termp/yolov5/runs/detect').glob('*'), key=os.path.getmtime, reverse=True)[0]
     image_basename = image_path.name
     image_name, image_extension = os.path.splitext(image_basename)
-    image_name = image_name  # 확장자 제외
 
     label_path = detect_dir / 'labels' / f'{image_name}.txt'
     
     if not label_path.is_file():
         raise FileNotFoundError("Object detection results not found")
     
-    with open(label_path, 'r') as file:
+    with open(label_path, 'r', encoding='cp949') as file:
         lines = file.readlines()
         detected_classes = sorted(set(line.split()[0] for line in lines))
+        
     
     color_keyword_array = []
     for cls in detected_classes:
